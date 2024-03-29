68e1db589f1ae9930a1f90c2ce699394b4a9a778
pavleen71 <149606091+pavleen71@users.noreply.github.com>
2024-03-29 11:37:21 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] x=new int[5] {1,3,4,6,7};
+            int sum = 0;
+            for(int i = 0; i < x.Length; i++)
+            {
+                
+                Console.WriteLine(x[i]);
+                sum = x[0] + x[x.Length - 1] + x[(x.Length) / 2];
+               
+
+            }
+            Console.WriteLine(sum);
+
+        }
+    }
+}
